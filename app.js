b4914e99248c48b88d819565ab5485b71ecd0a59
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-10-03 21:04:03 +0300

    added new lines

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // VARIABLES
 let body = document.querySelector('body');
+let info = document.querySelector('.info');
+let mercury = document.querySelector('.cls-146');
+let venus = document.querySelector('.cls-156');
+let earth = document.querySelector('.cls-127');
+
 
 
 // STAR RENDERING FUNCTIONS
@@ -8,6 +13,7 @@ function defaultSmallStars() {
     smallStars.setAttribute('id', 'stars');
     smallStars.style.left = Math.random() * 100 + 'vw';
     smallStars.style.top = Math.random() * 100 + 'vh';
+    smallStars.style.animationDuration = Math.random() * 20 + 25 + 's';
     body.appendChild(smallStars)
 }
 
@@ -16,25 +22,39 @@ function createSmallStars() {
     let smallStars = document.createElement('div');
     smallStars.setAttribute('id', 'stars');
     smallStars.style.left = Math.random() * 100 + 'vw';
+
+    smallStars.style.animationDuration = Math.random() * 15 + 20 + 's';
     body.appendChild(smallStars)
+    setTimeout(() => {
+        smallStars.remove()
+    }, 40000)  // changing this you might have to change animation duration in css
 }
 
+// EVENT LISTENERS
 
 // DISPLAY STARS
-
 window.addEventListener('DOMContentLoaded', () => {
-    for (let i = 0; i < 10; i++) {
-        createSmallStars()
-    }
+    // for (let i = 0; i < 10; i++) {
+    //     createSmallStars()
+    // }
 
-    for (let i = 0; i < 100; i++) {
-        defaultSmallStars()
-    }
+    // for (let i = 0; i < 100; i++) {
+    //     defaultSmallStars()
+    // }
 
-    setInterval(createSmallStars, 300)
+    setInterval(createSmallStars, 200)
 })
 
 
+
+
+
+
+
+
+// on planet click, display planet info
+
+// remove info if click outside planet
 
 
 
--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 // const url = 'https://api.le-systeme-solaire.net/rest/bodies/?data=id,englishName,isPlanet,mass,vol,density,gravity,equaRadius,aroundPlanet,discoveredBy,discoveryDate,avgTemp';
 
+let sunUrl = 'https://api.le-systeme-solaire.net/rest/bodies/soleil';
+
 const url = 'https://api.le-systeme-solaire.net/rest/bodies/';
 
 async function getData() {
@@ -7,16 +9,22 @@ async function getData() {
     const data = await response.json()
     return data
 }
+async function getSunData() {
+    const response = await fetch(sunUrl)
+    const data = await response.json()
+    console.log(data)
+}
 
 window.addEventListener('DOMContentLoaded', () => {
+    getSunData()
     getData()
-        .then(res => checkIfPlanet(res))
+        .then(res => getPlanets(res))   // do something here??? why do i want to console log the planets? i have to add the event listeners
 })
 
-function checkIfPlanet(data) {
+function getPlanets(data) {
     for (let i = 0; i < data.bodies.length; i++) {
         if (data.bodies[i].isPlanet === true) {
-            console.log(data.bodies[i]);
+            console.log(data.bodies[i])
         }
     }
 }
