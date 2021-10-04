290569c10a67874658210298fc8db5259fc2547f
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-10-04 11:54:16 +0300

    added show info functionality

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // VARIABLES
+
 let body = document.querySelector('body');
 let info = document.querySelector('.info');
+const svg = document.querySelector('svg')
 let mercury = document.querySelector('.cls-146');
 let venus = document.querySelector('.cls-156');
 let earth = document.querySelector('.cls-127');
 
+//FUNCTIONS
 
-
-// STAR RENDERING FUNCTIONS
+//star rendering function
 function defaultSmallStars() {
     let smallStars = document.createElement('div')
     smallStars.setAttribute('id', 'stars');
@@ -17,7 +19,7 @@ function defaultSmallStars() {
     body.appendChild(smallStars)
 }
 
-
+//star rendering function
 function createSmallStars() {
     let smallStars = document.createElement('div');
     smallStars.setAttribute('id', 'stars');
@@ -30,9 +32,74 @@ function createSmallStars() {
     }, 40000)  // changing this you might have to change animation duration in css
 }
 
+//display planet info function
+function showPlanets(arr) {
+
+    svg.addEventListener('click', (e) => {
+
+        for (let i = 0; i < arr.bodies.length; i++) {
+            if (e.target.id === arr.bodies[i].englishName && arr.bodies[i].moons !== null) {  //display planets that have moons
+                info.innerHTML = `
+                <h2 class="planet-name">${ e.target.id }</h2>
+                <p><span class="property">Mass</span><span class="value"> ${ arr.bodies[i].mass.massValue.toFixed(2) }<sup>${ arr.bodies[i].mass.massExponent }</sup>kg</span></p>
+                <p><span class="property">Volume</span><span class="value"> ${ arr.bodies[i].vol.volValue.toFixed(2) }<sup>${ arr.bodies[i].vol.volExponent }</sup> km<sup>3</sup></span></p>
+                <p><span class="property">Density</span><span class="value"> ${ arr.bodies[i].density.toFixed(2) } g/cm<sup>3</sup></span></p>
+                <p><span class="property">Radius</span><span class="value"> ${ numberWithCommas(arr.bodies[i].equaRadius.toFixed(2)) } km</span></p>
+                <p><span class="property">Gravity</span><span class="value"> ${ arr.bodies[i].gravity.toFixed(2) } m/s<sup>2</sup></span></p>
+                <p><span class="property">Moons</span><span class="value"> ${ arr.bodies[i].moons.length }</span></p>
+                `
+            } else if (e.target.id === arr.bodies[i].englishName && arr.bodies[i].moons === null && arr.bodies[i].vol !== null) { //display planets that dont have moons
+                info.innerHTML = `
+                <h2 class="planet-name">${ e.target.id }</h2>
+                <p><span class="property">Mass</span><span class="value"> ${ arr.bodies[i].mass.massValue.toFixed(2) }<sup>${ arr.bodies[i].mass.massExponent }</sup>kg</span></p>
+                <p><span class="property">Volume</span><span class="value"> ${ arr.bodies[i].vol.volValue.toFixed(2) }<sup>${ arr.bodies[i].vol.volExponent }</sup> km<sup>3</sup></span></p>
+                <p><span class="property">Density</span><span class="value"> ${ arr.bodies[i].density.toFixed(2) } g/cm<sup>3</sup></span></p>
+                <p><span class="property">Radius</span><span class="value"> ${ numberWithCommas(arr.bodies[i].equaRadius.toFixed(2)) } km</span></p>
+                <p><span class="property">Gravity</span><span class="value"> ${ arr.bodies[i].gravity.toFixed(2) } m/s<sup>2</sup></span></p>
+                <p><span class="property">Moons</span><span class="value"> None</span></p>
+                `
+            } else if (e.target.id === arr.bodies[i].englishName && arr.bodies[i].vol === null) {
+                info.innerHTML = `
+                <h2 class="planet-name">${ e.target.id }</h2>
+                <p><span class="property">Mass</span><span class="value"> ${ arr.bodies[i].mass.massValue.toFixed(2) }<sup>${ arr.bodies[i].mass.massExponent }</sup>kg</span></p>
+                <p><span class="property">Volume</span><span class="value"> 14.09<sup>18</sup> km<sup>3</sup></span></p>
+                <p><span class="property">Density</span><span class="value"> ${ arr.bodies[i].density.toFixed(2) } g/cm<sup>3</sup></span></p>
+                <p><span class="property">Radius</span><span class="value"> ${ numberWithCommas(arr.bodies[i].equaRadius.toFixed(2)) } km</span></p>
+                <p><span class="property">Gravity</span><span class="value"> 274.00 m/s<sup>2</sup></span></p>
+                <p><span class="property">Moons</span><span class="value"> None</span></p>
+                `
+            }
+        }
+    })
+    showGeneralInfo()
+}
+
+//show general info
+function showGeneralInfo() {
+    window.addEventListener('click', (e) => {
+        if (e.target.id == false) {
+            info.innerHTML = `
+            <h2 class="planet-name">The Solar System</h2>
+            <div class="solar-system-info">The planetary system we call home is located in an outer spiral arm of the Milky Way galaxy.
+                Our solar system consists of our star, the Sun, and everything bound to it by gravity – the planets Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune; dwarf planets such as Pluto; dozens of moons; and millions of asteroids, comets, and meteoroids.      
+                Beyond our own solar system, there are more planets than stars in the night sky. So far, we have discovered thousands of planetary systems orbiting other stars in the Milky Way, with more planets being found. Most of the hundreds of billions of stars in our galaxy are thought to have planets of their own, and the Milky Way is but one of perhaps 100 billion galaxies in the universe.       
+                While our planet is in some ways a mere speck in the vast cosmos, we have a lot of company out there. It seems that we live in a universe packed with planets – a web of countless stars accompanied by families of objects, perhaps some with life of their own. Our planetary system is named the "solar system" because our Sun is named Sol and anything related to the Sun we call "solar."</div>
+            `
+        }
+        // if (e.target !== svg.path) {
+        //     console.log('hello');
+        // }
+    })
+}
+
+// function that adds commas to thousands numbers
+function numberWithCommas(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // EVENT LISTENERS
 
-// DISPLAY STARS
+//display stars
 window.addEventListener('DOMContentLoaded', () => {
     // for (let i = 0; i < 10; i++) {
     //     createSmallStars()
@@ -48,11 +115,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
-
-// on planet click, display planet info
 
 // remove info if click outside planet
 
--- data.js
+++ data.js
@@ -4,27 +4,15 @@ let sunUrl = 'https://api.le-systeme-solaire.net/rest/bodies/soleil';
 
 const url = 'https://api.le-systeme-solaire.net/rest/bodies/';
 
+
+
 async function getData() {
     const response = await fetch(url)
     const data = await response.json()
-    return data
-}
-async function getSunData() {
-    const response = await fetch(sunUrl)
-    const data = await response.json()
-    console.log(data)
+    showPlanets(data)
 }
 
+//get data on page load
 window.addEventListener('DOMContentLoaded', () => {
-    getSunData()
     getData()
-        .then(res => getPlanets(res))   // do something here??? why do i want to console log the planets? i have to add the event listeners
 })
-
-function getPlanets(data) {
-    for (let i = 0; i < data.bodies.length; i++) {
-        if (data.bodies[i].isPlanet === true) {
-            console.log(data.bodies[i])
-        }
-    }
-}
